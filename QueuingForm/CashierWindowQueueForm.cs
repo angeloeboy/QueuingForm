0f3be68ec7e2fbfdd3da5b9ae41bb010bd4fef3b
Angelo <angeloeboy10@gmail.com>
2022-10-14 15:44:27 +0800

    Add project files.

---
+++ QueuingForm/CashierWindowQueueForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueuingForm
+{
+    public partial class CashierWindowQueueForm : Form
+    {
+
+        CustomerView customerView = new CustomerView();
+        public CashierWindowQueueForm()
+        {
+            InitializeComponent();
+            customerView.Show();
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+        }
+
+        public void DisplayCashierQueue(IEnumerable CashierList)
+        {
+            //clears the view list and readds value by looping through queue provided by the parameter
+            listCashierQueue.Items.Clear();
+            foreach (Object obj in CashierList)
+            {
+                listCashierQueue.Items.Add(obj.ToString());
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            //dequeue lols
+            CashierClass.CashierQueue.Dequeue();
+            customerView.UpdateService(CashierClass.CashierQueue.Peek());
+        }
+
+        private void CashierWindowQueueForm_Load(object sender, EventArgs e)
+        {
+
+            //runs RefreshList method contiuously
+            Timer timer = new Timer();
+            timer.Interval = (1 * 1000);
+            timer.Tick += new EventHandler(RefreshList);
+            timer.Start();
+
+        }
+
+        private void RefreshList(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+        }
+    }
+}
---
+++ QueuingForm/CustomerView.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace QueuingForm
+{
+    partial class CustomerView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblQueue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "*Now Serving";
+            // 
+            // lblQueue
+            // 
+            this.lblQueue.AutoSize = true;
+            this.lblQueue.Font = new System.Drawing.Font("Microsoft Sans Serif", 34.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQueue.Location = new System.Drawing.Point(35, 131);
+            this.lblQueue.Name = "lblQueue";
+            this.lblQueue.Size = new System.Drawing.Size(235, 67);
+            this.lblQueue.TabIndex = 3;
+            this.lblQueue.Text = "Position";
+            // 
+            // CustomerView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 260);
+            this.Controls.Add(this.lblQueue);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomerView";
+            this.Text = "CustomerView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblQueue;
+    }
+}
